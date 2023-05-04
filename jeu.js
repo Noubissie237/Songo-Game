243eafb3b5355f544ead24bd94ad3049fbd81799
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2023-05-04 06:56:15 +0100

    Songo2

--- jeu.js
+++ jeu.js
@@ -1,3 +1,7 @@
+
+let permission1 = 0 // permission de jouer du joueur 1, il est donc le deuxieme a jouer
+let permission2 = 1 // permission de jouer du joueur 2, il est donc le premier a jouer
+
 class Songo
 {
     constructor(Cj1, Cj2, pt1, pt2)
@@ -20,10 +24,25 @@ class Songo
 
     estBloque(idj)  // prototype permettant de savoir si le joueur dont l'id est passé en paramettre est bloqué en retournant 0 ou pas en retournant 1
     {
+        if(idj == 1)
+            if(permission1 == 0)
+                return 0
+            else
+                return 1
+        else
+            if(permission2 == 0)
+                return 0
+            else
+                return 1
+    }
+
+    distribution(idj) // prototype permettant d'effectuer la distribution des pions par le joueur dont l'id est passé en paramettre
+    {
         
     }
 
 }
+
 
 function init()
 {
@@ -33,6 +52,7 @@ function init()
     for(let j = 1; j <= 2; j++)
         for(let i = 1; i<= 7; i++)
             document.getElementById("J"+j+"pionsCase"+i).value = 5
+    
 }
 
 /*function debut()
@@ -44,4 +64,15 @@ function init()
 
     ctx.fillRect(0,0,Largeur,Longueur)
 
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML = this.responseText;
+    }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+}
 }*/
