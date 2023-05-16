f88a8370f556daa5ca7a0401ff7a478d84d46ce3
Noubissie237 <noubissiekamgawilfried2017@gmail.com>
2023-05-16 02:50:04 +0100

    user2 ok

--- UU/insertEvol2_3.php
+++ UU/insertEvol2_3.php
@@ -4,8 +4,7 @@
     $point = $_GET['val'];
     $tmp = $point;
     $case = 3;
-    $pos = array();
-    $ying = 0;
+    $pos = 1;
     $pos1 = $pos-1;
     $continue = 1;
 
@@ -39,13 +38,53 @@
                     $res = $valeur['nbrePions'];
                 $res += 1;
                 $req = $mysqli->query("UPDATE joueur1 SET nbrePions=$res WHERE idCase=$i");
-                $pos[$ying] = 1;
-                $ying ++;
+                $pos += 1;
             }
-            else
+        }
+    
+        $pf = $pos-3;
+
+        for($j=$pf; $j>=1; $j--)
+        {
+            $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$j");
+            foreach($req as $valeu)
+                $res26 = $valeu['nbrePions'];
+            if($res26!=2 and $res26!=3 and $res26!=4)
             {
-                $pf = count($pos);
-                $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$pf");
+                $continue = 0;
+            }
+            if($continue == 1)
+            {
+                $up1 = $mysqli->query("SELECT * FROM score WHERE id=2");
+                foreach($up1 as $u)
+                    $sc = $u['scores'];
+                $sc += $res26;
+                $up = $mysqli->query("UPDATE score SET scores=$sc WHERE id=2");
+                $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$j");
+            }
+        }    
+            $point -= 1; 
+    }
+
+   if($point > 0)
+    { $pos = 1;
+        for($i = 7; $i >= $case; $i++)
+        {
+            if($point > 0)
+            {
+                $req1 = $mysqli->query("SELECT * FROM joueur2 WHERE idCase=$i");
+
+                foreach($req1 as $valeur)
+                    $res = $valeur['nbrePions'];
+                $res += 1;
+                $req = $mysqli->query("UPDATE joueur2 SET nbrePions=$res WHERE idCase=$i");
+                $pos++;
+            }
+            $pf = $pos-3;
+
+            for($j=$pf; $j>=1; $j--)
+            {
+                $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$j");
                 foreach($req as $valeu)
                     $res26 = $valeu['nbrePions'];
                 if($res26!=2 and $res26!=3 and $res26!=4)
@@ -59,27 +98,9 @@
                         $sc = $u['scores'];
                     $sc += $res26;
                     $up = $mysqli->query("UPDATE score SET scores=$sc WHERE id=2");
-                    $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$pf");
+                    $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$j");
                 }
-            }
-            $point -= 1;
-        }
-    }
-
-    if($point > 0)
-    {
-        for($i = 7; $i > $case; $i++)
-        {
-            if($point > 0)
-            {
-                $req1 = $mysqli->query("SELECT * FROM joueur2 WHERE idCase=$i");
-
-                foreach($req1 as $valeur)
-                    $res = $valeur['nbrePions'];
-                $res += 1;
-                $req = $mysqli->query("UPDATE joueur2 SET nbrePions=$res WHERE idCase=$i");
-            }
-        
+            }    
         $point -= 1;
         }
     }
--- UU/insertEvol2_6.php
+++ UU/insertEvol2_6.php
@@ -4,8 +4,7 @@
     $point = $_GET['val'];
     $tmp = $point;
     $case = 6;
-    $pos = array();
-    $ying = 0;
+    $pos = 1;
     $pos1 = $pos-1;
     $continue = 1;
 
@@ -39,34 +38,75 @@
                     $res = $valeur['nbrePions'];
                 $res += 1;
                 $req = $mysqli->query("UPDATE joueur1 SET nbrePions=$res WHERE idCase=$i");
-                $pos[$ying] = 1;
-                $ying ++;
+                $pos += 1;
             }
-            else
-            {
-                $pf = count($pos);
-                $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$pf");
-                foreach($req as $valeu)
-                    $res26 = $valeu['nbrePions'];
-                if($res26!=2 and $res26!=3 and $res26!=4)
-                {
-                    $continue = 0;
-                }
-                if($continue == 1)
-                {
-                    $up1 = $mysqli->query("SELECT * FROM score WHERE id=2");
-                    foreach($up1 as $u)
-                        $sc = $u['scores'];
-                    $sc += $res26;
-                    $up = $mysqli->query("UPDATE score SET scores=$sc WHERE id=2");
-                    $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$pf");
-                }
-            }
-            $point -= 1;
         }
+    
+        $pf = $pos-3;
+
+        for($j=$pf; $j>=1; $j--)
+        {
+            $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$j");
+            foreach($req as $valeu)
+                $res26 = $valeu['nbrePions'];
+            if($res26!=2 and $res26!=3 and $res26!=4)
+            {
+                $continue = 0;
+            }
+            if($continue == 1)
+            {
+                $up1 = $mysqli->query("SELECT * FROM score WHERE id=2");
+                foreach($up1 as $u)
+                    $sc = $u['scores'];
+                $sc += $res26;
+                $up = $mysqli->query("UPDATE score SET scores=$sc WHERE id=2");
+                $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$j");
+            }
+        }    
+            $point -= 1; 
     }
 
     if($point > 0)
+    {
+        for($i = 1; $i < 7; $i++)
+        {
+            if($point > 0)
+            {
+                $req1 = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$i");
+    
+                foreach($req1 as $valeur)
+                    $res = $valeur['nbrePions'];
+                $res += 1;
+                $req = $mysqli->query("UPDATE joueur1 SET nbrePions=$res WHERE idCase=$i");
+                $pos += 1;
+            }
+        }
+    
+        $pf = $pos-3;
+
+        for($j=$pf; $j>=1; $j--)
+        {
+            $req = $mysqli->query("SELECT * FROM joueur1 WHERE idCase=$j");
+            foreach($req as $valeu)
+                $res26 = $valeu['nbrePions'];
+            if($res26!=2 and $res26!=3 and $res26!=4)
+            {
+                $continue = 0;
+            }
+            if($continue == 1)
+            {
+                $up1 = $mysqli->query("SELECT * FROM score WHERE id=2");
+                foreach($up1 as $u)
+                    $sc = $u['scores'];
+                $sc += $res26;
+                $up = $mysqli->query("UPDATE score SET scores=$sc WHERE id=2");
+                $up2 = $mysqli->query("UPDATE joueur1 SET nbrePions=0 WHERE idCase=$j");
+            }
+        }    
+            $point -= 1; 
+    }
+
+/*   if($point > 0)
     {
         for($i = 7; $i > $case; $i++)
         {
@@ -82,5 +122,5 @@
         
         $point -= 1;
         }
-    }
+    }*/
 
